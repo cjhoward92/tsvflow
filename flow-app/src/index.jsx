3807de389268efbe1b3a9c1991f55d4d5293a05c
Carson Howard <cjhoward92@gmail.com>
2018-08-25 11:22:02 -0700

    Updated stuff for talk

--- flow-app/src/index.jsx
+++ flow-app/src/index.jsx
@@ -7,6 +7,40 @@ import store from './redux/store';
 
 import App from './App.jsx';
 
+declare var $: number;
+
+type A = {
+  a: boolean
+};
+
+type B = {
+  b: number
+};
+
+type C = { ...A, ...B };
+
+const obj1: A = {
+  a: true
+};
+
+const obj2: B = {
+  b: 10
+};
+
+const obj3: C = {
+  ...obj1,
+  ...obj2
+};
+
+type FnType = <T>(...rest: T[]) => *;
+const fn: FnType = (...rest) => rest[0];
+
+const x = $;
+const numFunc = (n: number) => n + 1;
+const y = numFunc(x);
+
+process.exit();
+
 const rootElement: HTMLElement | null = document.getElementById('root');
 if (!rootElement) {
   throw new Error('What?');
--- ts-app/src/index.tsx
+++ ts-app/src/index.tsx
@@ -5,6 +5,37 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
+declare var $: number;
+
+type A = {
+  a: boolean;
+};
+
+type B = {
+  b: number;
+};
+
+const obj1: A = {
+  a: true
+};
+
+const obj2: B = {
+  b: 10
+};
+
+type C = A & B;
+
+const obj3: C = {
+  ...obj1,
+  ...obj2
+};
+
+const x = $;
+const numFunc = (n: number) => n + 1;
+const y = numFunc(x);
+
+process.exit(1);
+
 // This doesn't throw an error because the types for react-dom are better
 ReactDOM.render(
   <Provider store={store}>
